b5717c21952dd3a545f9671121b423f1b9d095ef
Dan Baugh <daniel.r.baugh@gmail.com>
2020-06-12 00:19:36 -0400

    Topic - New, Index, Save to DB

---
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -0,0 +1,49 @@
+defmodule DiscussWeb.TopicController do
+  use DiscussWeb, :controller
+
+  alias Discuss.Topic
+  alias Discuss.Repo
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render(conn, "index.html", topics: topics)
+  end
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"topic" => topic_params}) do
+    changeset = Topic.changeset(%Topic{}, topic_params)
+
+    case Repo.insert(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic created successfully.")
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  # def create_user(attrs \\ %{}) do
+  #   %User{}
+  #   |> User.changeset(attrs)
+  #   |> Repo.insert()
+  # end
+
+  # def create(conn, %{"user" => user_params}) do
+  #   case Accounts.create_user(user_params) do
+  #     {:ok, user} ->
+  #       conn
+  #       |> put_flash(:info, "User created successfully.")
+  #       |> redirect(to: Routes.user_path(conn, :show, user))
+  #
+  #     {:error, %Ecto.Changeset{} = changeset} ->
+  #       render(conn, "new.html", changeset: changeset)
+  #   end
+  # end
+end
