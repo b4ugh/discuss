0c2b11e06308e4453a763e9d2f1201836b8ee221
Dan Baugh <daniel.r.baugh@gmail.com>
2020-06-12 12:51:54 -0400

    edit and delete topics

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -15,8 +15,8 @@ defmodule DiscussWeb.TopicController do
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"topic" => topic_params}) do
-    changeset = Topic.changeset(%Topic{}, topic_params)
+  def create(conn, %{"topic" => topic_attrs}) do
+    changeset = Topic.changeset(%Topic{}, topic_attrs)
 
     case Repo.insert(changeset) do
       {:ok, _topic} ->
@@ -29,21 +29,35 @@ defmodule DiscussWeb.TopicController do
     end
   end
 
-  # def create_user(attrs \\ %{}) do
-  #   %User{}
-  #   |> User.changeset(attrs)
-  #   |> Repo.insert()
-  # end
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
 
-  # def create(conn, %{"user" => user_params}) do
-  #   case Accounts.create_user(user_params) do
-  #     {:ok, user} ->
-  #       conn
-  #       |> put_flash(:info, "User created successfully.")
-  #       |> redirect(to: Routes.user_path(conn, :show, user))
-  #
-  #     {:error, %Ecto.Changeset{} = changeset} ->
-  #       render(conn, "new.html", changeset: changeset)
-  #   end
-  # end
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic_attrs}) do
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic_attrs)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic updated successfully.")
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "edit.html", changeset: changeset, topic: old_topic)
+    end
+  end
+
+  def delete(conn, %{"id" => topic_id}) do
+    Topic
+    |> Repo.get!(topic_id)
+    |> Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic deleted successfully.")
+    |> redirect(to: Routes.topic_path(conn, :index))
+  end
 end
